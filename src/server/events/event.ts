5666d663c19cf28757d5e67bf8531272bf1255ae
night_ming <961880295@qq.com>
2024-07-09 13:45:46 +0800

    eslint测试

--- src/server/events/event.ts
+++ src/server/events/event.ts
@@ -1,7 +1,7 @@
 import { handles } from '@server/events/handles'
 import { isGroupActive, joinGroup, mountUserInfo, switchGroup } from '@server/events/common'
 import { setData } from '@server/global'
-import type { event } from '../type/type'
+import type { event, msg } from '../type/type'
 
 const events: event[] = [
   ...handles,
@@ -19,7 +19,7 @@ function matches(event: event, content: string): boolean {
   return false
 }
 
-async function triggerEvent(data: string) {
+async function triggerEvent(data: msg) {
   // 处理开关群事件
   await switchGroup(data)
   // 判断是否开启了群聊
