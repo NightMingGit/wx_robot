0f12edfc1308ea9a71581ad6056c5e96da126649
NightMing <961880295@qq.com>
2024-07-14 10:46:39 +0800

    1

--- src/server/api/system.ts
+++ src/server/api/system.ts
@@ -7,16 +7,23 @@ export function sendText(msg: string, receiver: string, aters: string = '') {
     aters,
   })
 }
+
 export function sql(db: string, sql: string) {
   return post('/sql', {
     db,
     sql,
   })
 }
+
 // 机器人信息
 export function userinfo() {
   return get('/userinfo', {})
 }
+
 export function test() {
   return get('https://api.yujn.cn/api/kemu.php?type=json', {})
+}
+
+export function sendBody(data: any) {
+  return post('http://127.0.0.1:5000', data)
 }
--- src/server/main.ts
+++ src/server/main.ts
@@ -6,12 +6,20 @@ import '@server/models/index'
 import '@server/job/index'
 import { triggerEvent } from '@server/events/event'
 import type { msg } from '@server/type/type'
+import { sendBody } from '@server/api/system'
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.post('/robot', (req, res) => {
   triggerEvent(req.body as msg)
+  // 转发给py
+  try {
+    sendBody(req.body)
+  }
+  catch (e) {
+    console.log('转发py失败')
+  }
   res.send({
     status: '0',
     message: 'success',
